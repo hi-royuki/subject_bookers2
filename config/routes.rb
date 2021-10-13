5be0b0b7ac2137f14ce565aa6202e0d878fa19ae
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-13 15:59:32 +0000

    作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  resources :books
+  resources :homes
+  resources :users
+  
+  get 'home/about' => 'homes#show', as: 'about'
+  # どのURLにアクセスしたときに、どのコントローラのどのアクションを呼び出すか(get 'url' => アクション)
+  
+  
 end
